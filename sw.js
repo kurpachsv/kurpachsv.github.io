04f6e7b8f66632f62516a049d04dbc5f582574f0
kurpachsv <kurpachsv@gmail.com>
2018-04-30 18:14:48 +0300

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-5329ec3753e9d17c0cf6.js","2791f362177abf9b683175588c382e18"],["/commons-1d68d573cb2a67604378.js","eb61ae03bc62e315976bde69d3f7f79b"],["/index.html","eff29dfddfb17f680ad813aeedaf74a5"],["/offline-plugin-app-shell-fallback/index.html","35f5e5cf4cf334e234be9695d501c15e"]];
+var precacheConfig = [["/app-5329ec3753e9d17c0cf6.js","2791f362177abf9b683175588c382e18"],["/commons-1d68d573cb2a67604378.js","eb61ae03bc62e315976bde69d3f7f79b"],["/index.html","3594c1cd51e56c51d241d5aa291bff51"],["/offline-plugin-app-shell-fallback/index.html","db947d2785997f6dd5a98228e68c5649"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
